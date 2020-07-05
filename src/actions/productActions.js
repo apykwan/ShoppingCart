cde9288a28beb1747201f878903db6c32a326ad4
apkwan <apkwan317@gmail.com>
2020-07-05 13:28:39 -0700

    add redux to cart

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -1,5 +1,9 @@
-import { FETCH_PRODUCTS } from "../types";
-import { FILTER_PRODUCTS_BY_SIZE, ORDER_PRODUCTS_BY_PRICE } from "../types";
+import {
+    FETCH_PRODUCTS,
+    FILTER_PRODUCTS_BY_SIZE,
+    ORDER_PRODUCTS_BY_PRICE,
+} from "../types";
+
 export const fetchProducts = () => async (dispatch) => {
     const res = await fetch("/api/products");
     const data = await res.json();
@@ -22,6 +26,7 @@ export const filterProducts = (products, size) => (dispatch) => {
         },
     });
 };
+
 export const sortProducts = (filteredProducts, sort) => (dispatch) => {
     const sortedProducts = filteredProducts.slice();
     if (sort === "latest") {
